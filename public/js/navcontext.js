45a2b02abf87cdb830564d9ef3c175b5e7976577
Gargouille <792459576@qq.com>
2016-03-03 19:40:39 +0800

    fix somebug and add to blog

--- public/js/navcontext.js
+++ public/js/navcontext.js
@@ -3,27 +3,23 @@
  */
 function initNavContext(){
     var contextBar = $($("#navbar-context").children()[0]);
-    console.log(contextBar);
-    var length = 0;
+    var lastItem = $(contextBar.children()[contextBar.children().length-1]);
     var h3s = $(".note-editable").find("h3");
     h3s.each(function(key,elem){
         var itemText = $(elem).text();
         var itemTextArray = itemText.split(".");
-        itemTextArray.shift();
+        if(itemTextArray.length>1){
+            itemTextArray.shift();
+        }
         itemText = itemTextArray.toString().trim();
+        console.log(itemText);
         if(itemText!=""){
-            var timestamp = Date.parse(new Date())/1000;
+            var date  = new Date();
+            var timestamp = Date.parse(date)/1000;
             var itemHref = "context"+key+"at"+timestamp;
-            if(elem.id==itemHref){
-
-            }
             elem.id = itemHref;
             itemHref = "#"+itemHref;
-            contextBar.append($("<li role='presentation'><a href="+itemHref+">"+itemText+"</a></li>"));
-        }
-        length = key;
-        if(length>=h3s.toArray().length-1){
-            contextBar.append($("<li role='presentation'><a href='#bottom'>Bottom</a></li>"));
+            lastItem.before($("<li role='presentation'><a href="+itemHref+">"+itemText+"</a></li>"));
         }
     });
 
